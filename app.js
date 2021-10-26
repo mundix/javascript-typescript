cd1e67dc51ec3cc417489706af396eeec6a8709f
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-26 05:28:51 -0400

    Exercise #2 passed

--- app.js
+++ app.js
@@ -32,4 +32,47 @@
     var poder = '100';
     var largoDelPoder = poder === null || poder === void 0 ? void 0 : poder.length;
     console.log(largoDelPoder);
+    // !~-----------------------------
+    // * Ejercicio Practivo de Seccion Functions 
+    // !~-----------------------------
+    //? Funciones básicas
+    // function sumar(a, b) {
+    //   return a + b;
+    // }
+    var sumar = function (a, b) {
+        return a + b;
+    };
+    // var contar = (heroes) => {
+    //   return heroes.length;
+    // }
+    var contar = function (heroes) {
+        return heroes.length;
+    };
+    var superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+    contar(superHeroes);
+    // Parametros por defecto
+    // const llamarBatman = (llamar) => {
+    //   if (llamar) {
+    //     console.log("Batiseñal activada");
+    //   }
+    // }
+    var llamarBatman = function (llamar) {
+        if (llamar === void 0) { llamar = false; }
+        if (llamar) {
+            console.log("Batiseñal activada");
+        }
+    };
+    llamarBatman();
+    //Rest?
+    var unirheroes = function () {
+        var personas = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            personas[_i] = arguments[_i];
+        }
+        return personas.join(", ");
+    };
+    //Tipo funcion
+    var noHaceNada = function (numero, texto, booleano, arreglo) { };
+    // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+    var noHaceNadaTampoco = noHaceNada(1, 'Hola', true, superHeroes);
 })();
